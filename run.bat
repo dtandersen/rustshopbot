3eeda58c574a04eda2e602d6c3713398e2709b60
David Andersen <git@davidandersen.us>
2024-11-02 22:44:37 -0700

    build and run scripts

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-docker build -t rustshopbot .
+@REM docker build -t rustshopbot .
 rem docker run --rm -it -v %cd%\json\config.json:/app/json/config.json rustshopbot bash
 docker run --rm -it -v %cd%\json\config.json:/app/json/config.json rustshopbot python Discordbot.py
