6b874932c77e88791984b5f40cb7998e9d2605d2
David Andersen <git@davidandersen.us>
2024-11-09 12:39:34 -0800

    remove unused imports

--- tests/test_find_items.py
+++ tests/test_find_items.py
@@ -2,28 +2,19 @@ from typing import List
 from hamcrest import (
     assert_that,
     contains_inanyorder,
-    equal_to,
 )
-import pytest
 
 from rustshopbot.command.search_items import SearchItems
-from rustshopbot.command.search_sellers import SearchSellers
-from rustshopbot.entity.order import Order
-from rustshopbot.gateway.rust import RustOrder, ServerConfig
 from rustshopbot.repository.item_repository import Item, JsonItemRepository
-from rustshopbot.repository.server_repository import JsonServerRepository
-from tests.fakes import FakeRustGateway, FakeRustGatewayFactory
 
 
-# @pytest.mark.asyncio
 class TestFindItems:
     def test_answer(self):
         items = JsonItemRepository()
 
-        command = SearchItems(
-            items=JsonItemRepository(),
-        )
+        command = SearchItems(items=items)
         result = command.execute(query="jack", limit=2)
+
         assert_that(
             result,
             contains_inanyorder(
